4d4aa64e164c324677f465298df2b9e6dc2c991a
Nik Mirza <nik96mirza@gmail.com>
2019-09-21 18:52:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple-File-Service
+
+A micro service to quickly help me setup a mock file upload server. What it does? Upload and serve file.
+
+## Usage
+
+install :
+
+`npm install -g simple-file-service`
+
+run :
+
+`simple-file-service -p 3000 -h localhost -d uploads`
+
+options :
+
+`-p --port` define port.
+
+`-h --host` define host.
+
+`-d --directory` define serving directory.
---
+++ server.js
@@ -0,0 +1,48 @@
+const program = require('commander');
+const express = require('express');
+const multer  = require('multer');
+const fs  = require('fs');
+
+const app = require('./package.json');
+
+// Commander
+program.version(app.version);
+
+program
+    .option('-d, --directory <dir>', 'directory to serve', 'uploads')
+    .option('-p, --port <port>', 'port to serve', 3000)
+    .option('-h, --host <host>', 'hostname', '127.0.0.1')
+    .parse(process.argv);
+
+const dir = program.directory;
+const port = program.port;
+const host = program.host;
+
+// Express
+const server = express();
+server.use(express.static(dir))
+
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        //var dir = './uploads';
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+        callback(null, dir);
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+var upload = multer({storage: storage}).array('files', 12);
+server.post('/upload', function (req, res, next) {
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Something went wrong:(");
+        }
+        res.end("Upload completed.");
+    });
+})
+
+
+server.listen(port, host);
